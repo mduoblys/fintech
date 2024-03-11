9633a899497904b38822e500d378bbb2011337f7
mindaugasduoblys <mindaugasd@outlook.com>
2024-03-11 20:15:48 +0200

    Added Button and TextView functionality

--- app/src/main/java/lt/vu/fintech/MainActivity.kt
+++ app/src/main/java/lt/vu/fintech/MainActivity.kt
@@ -1,43 +1,42 @@
 package lt.vu.fintech
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import lt.vu.fintech.ui.theme.MyApplicationTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    initContent()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
+    fun initContent()
+    {
+        setContentView(R.layout.mainlayout)
+        prepareTextButton()
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
+    fun prepareTextButton()
+    {
+        findViewById<Button>(R.id.show_text_button)
+            .setOnClickListener {
+                val textView = findViewById<View>(R.id.appearing_text) as TextView
+                textView.text = "Text appeared"
+            }
     }
 }
